cbc85118c51dab79554b8eb276d58ba494a5abe3
ATerhaerdt <adam.terhaerdt@comcast.net>
2020-01-23 21:20:35 +0000

    transitionC

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
-$("transition").click(function(){
+var btn = $("#transition");
+
+btn.click(function(){
     console.log("Click");
-})
+    $("#bar1").animate({height:"100%"},2000).delay(100).animate({height:0},2000);
+    $("#bar2").delay(200).animate({height:"100%"},2000).delay(100).animate({height:0},2000);
+    $("#bar3").delay(400).animate({height:"100%"},2000).delay(100).animate({height:0},2000);
+    $("#bar4").delay(600).animate({height:"100%"},2000).delay(100).animate({height:0},2000);
+});
